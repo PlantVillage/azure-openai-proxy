e146f0d1ad2b31f24d3b34e9c0554da8ff325e13
Derek Morr <derekmorr@psu.edu>
2026-03-13 16:54:01 -0400

    add missing files

--- make_proxy.sh
+++ make_proxy.sh
@@ -2,4 +2,4 @@
 
 docker system prune -f
 docker build -t azure-openai-proxy:latest -f Dockerfile .
-docker run -p 8080:8080 --name=azure-openai-proxy -v ./config.yaml:/app/config.yaml azure-openai-proxy:latest
+docker run -p 8282:8080 --name=azure-openai-proxy -v ./config.yaml:/app/config.yaml azure-openai-proxy:latest
